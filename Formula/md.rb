03f37077a895999642454adadcb0546dcb4c2efe
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-06 16:38:35 +0000

    md: fix build under Xcode 12

--- Formula/md.rb
+++ Formula/md.rb
@@ -17,7 +17,9 @@ class Md < Formula
 
   def install
     cd "md" do
-      system ENV.cc, ENV.cflags, "-o", "md", "md.c"
+      # Xcode 12 made -Wimplicit-function-declaration an error by default so we need to
+      # disable that warning to successfully compile:
+      system ENV.cc, ENV.cflags, "-o", "md", "-Wno-implicit-function-declaration", "md.c"
       bin.install "md"
       man1.install "md.1"
     end
