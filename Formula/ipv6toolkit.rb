72be10d85375b7d21366432ee460324e0653dd0b
FX Coudert <fxcoudert@gmail.com>
2020-12-07 10:35:18 +0100

    Update ipv6toolkit.rb

--- Formula/ipv6toolkit.rb
+++ Formula/ipv6toolkit.rb
@@ -1,10 +1,9 @@
 class Ipv6toolkit < Formula
   desc "Security assessment and troubleshooting tool for IPv6"
-  homepage "https://www.si6networks.com/tools/ipv6toolkit/"
+  homepage "https://www.si6networks.com/research/tools/ipv6toolkit/"
   url "http://pages.cs.wisc.edu/~plonka/ipv6toolkit/ipv6toolkit-v2.0.tar.gz"
   sha256 "16f13d3e7d17940ff53f028ef0090e4aa3a193a224c97728b07ea6e26a19e987"
   license "GPL-3.0-or-later"
-
   head "https://github.com/fgont/ipv6toolkit.git"
 
   bottle do
