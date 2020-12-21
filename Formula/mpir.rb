17a3b33a8ef75faafb965c41e45b1b2f188023c1
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-21 13:04:41 +0000

    switch these to https to make CI happy

--- Formula/mpir.rb
+++ Formula/mpir.rb
@@ -1,7 +1,7 @@
 class Mpir < Formula
   desc "Multiple Precision Integers and Rationals (fork of GMP)"
-  homepage "http://mpir.org/"
-  url "http://mpir.org/mpir-3.0.0.tar.bz2"
+  homepage "https://mpir.org/"
+  url "https://mpir.org/mpir-3.0.0.tar.bz2"
   sha256 "52f63459cf3f9478859de29e00357f004050ead70b45913f2c2269d9708675bb"
   license "GPL-3.0-or-later"
 
